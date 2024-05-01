a2050e8adbf4c5c4f9a3d48bcf8893565349186a
PUO <86181292+kl204@users.noreply.github.com>
2024-05-01 18:55:50 +0900

    first commit

---
+++ src/main/java/com/example/eduprojjpa/EduprojJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.eduprojjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduprojJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduprojJpaApplication.class, args);
+    }
+
+}
